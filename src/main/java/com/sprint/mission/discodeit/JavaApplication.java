abdb35d5dd2c7d887776560ba8d0a15012005b2f
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-03-20 13:51:31 +0900

    feat: 메세지 엔티티 필드 추가

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -6,32 +6,29 @@ import com.sprint.mission.discodeit.service.UserService;
 
 public class JavaApplication {
     public static void main(String[] args) {
-        System.out.println("---------- 사용자 점검 ----------------\n");
-        UserService userService = new JCFUserService();
 
+        System.out.println("------------ 사용자 점검 ----------------\n");
+        UserService userService = new JCFUserService();
         User user1 = new User("test", "hsa123", "test123@gmail.com", "test", "http://test.com/profile.jpg","+8210-1234-5678");
         User user2 = new User("user2", "pass2", "user2@gmail.com", "닉네임2", "http://test.com/profile.jpg","+8210-1234-5678");
         User user3 = new User("user3", "pass3", "user3@gmail.com", "닉네임3",  "http://test.com/profile.jpg","+8210-1234-5678");
         userService.save(user1);
         userService.save(user2);
         userService.save(user3);
-
         System.out.println("------------ 단건 조회 ---------------\n");
         System.out.println(userService.findById(user1.getId()));
-
         System.out.println("------------ 다건 조회 ---------------\n");
         System.out.println(userService.findAll());
-
         User updateData = new User("updatedUser", "newPass!", "updated@gmail.com", "새닉네임", "http://test.com/profile.jpg", "+8210-1234-5678");
         userService.update(user1.getId(), updateData);
-
         System.out.println("------------- 유저 업데이트 -------------\n");
         System.out.println(userService.findAll());
-
         System.out.println("------------- 유저 삭제 -------------\n");
         userService.deleteById(user1.getId());
         System.out.println(userService.findAll());
+        System.out.println("-----------사용자 테스트 끝 -------------------\n");
 
-        System.out.println("--------- 테스트 끝 -------------------");
+        System.out.println("------------ 메시지 점검 ----------------\n");
+
     }
 }
--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,4 +1,64 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 public class Message {
+    private UUID messageId;
+    private List<Message> message;
+    private List<User> admin;
+    private List<User> user;
+    private String profileImageUrl;
+    private Long createdAt;
+    private Long updatedAt;
+
+    public Message(UUID messageId, List<Message>message, List<User> admin, List<User> user, String profileImageUrl ) {
+        this.messageId = messageId;
+        this.message = message;
+        this.admin = admin;
+        this.user = user;
+        this.profileImageUrl = profileImageUrl;
+        this.createdAt = System.currentTimeMillis();
+        this.updatedAt = System.currentTimeMillis();
+    }
+
+    public UUID getMessageId() {
+        return messageId;
+    }
+
+    public List<Message> getMessage() {
+        return message;
+    }
+
+    public List<User> getAdmin() {
+        return admin;
+    }
+
+    public List<User> getUser() {
+        return user;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void updateMessage(UUID messageId,Message message, User admin, User user, String profileImageUrl) {
+        this.messageId = messageId;
+        this.message = Arrays.asList(message);
+        this.admin = Arrays.asList(admin);
+        this.user = Arrays.asList(user);
+        this.profileImageUrl = profileImageUrl;
+        this.updatedAt = System.currentTimeMillis();
+    }
+
+
 }
