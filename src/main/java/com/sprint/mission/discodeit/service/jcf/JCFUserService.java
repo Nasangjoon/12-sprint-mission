c63ba322c49cef44b9717f989ca31e80fb0cff43
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-03-27 15:50:02 +0900

    refactor: 파일 구조 생성

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -1,4 +1,4 @@
-package com.sprint.mission.discodeit.jcf;
+package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.UserService;
