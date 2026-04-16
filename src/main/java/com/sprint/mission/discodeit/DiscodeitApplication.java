bb114f2b0ce7034b9732aa0939edeb2b7f74f2af
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-04-16 12:40:39 +0900

    refactor : DiscodeitApplication 변경

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -20,12 +20,44 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.UUID;
+import static com.sprint.mission.discodeit.JavaApplication.*;
+
 
 @SpringBootApplication
 public class DiscodeitApplication {
+    static User setupUser(UserService userService) {
+        User user = userService.create("woody", "woody@codeit.com", "woody1234");
+        return user;
+    }
+
+    static Channel setupChannel(ChannelService channelService) {
+        Channel channel = channelService.create(ChannelType.PUBLIC, "공지", "공지 채널입니다.");
+        return channel;
+    }
+
+    static void messageCreateTest(MessageService messageService, Channel channel, User author) {
+        Message message = messageService.create("안녕하세요.", channel.getId(), author.getId());
+        System.out.println("메시지 생성: " + message.getId());
+    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
-    }
+
+            UserRepository userRepository = new FileUserRepository();
+            ChannelRepository channelRepository = new FileChannelRepository();
+            MessageRepository messageRepository = new FileMessageRepository();
+
+            // 서비스 초기화
+            UserService userService = new BasicUserService(userRepository);
+            ChannelService channelService = new BasicChannelService(channelRepository);
+            MessageService messageService = new BasicMessageService(messageRepository, channelRepository, userRepository);
+
+            // 셋업
+            User user = setupUser(userService);
+            Channel channel = setupChannel(channelService);
+            // 테스트
+            messageCreateTest(messageService, channel, user);
+        }
+
+
 }
