313b476e951cc2f391148a149a91d0bf6f2dfdd0
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-04-16 12:43:48 +0900

    feat : Add Service Repository Bean

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileChannelRepository.java
@@ -2,6 +2,9 @@ package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
+import lombok.Getter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Repository;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -11,6 +14,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
+@ConditionalOnProperty(name = "discodeit.repository.type", havingValue = "file")
 public class FileChannelRepository implements ChannelRepository {
     private final Path DIRECTORY;
     private final String EXTENSION = ".ser";
