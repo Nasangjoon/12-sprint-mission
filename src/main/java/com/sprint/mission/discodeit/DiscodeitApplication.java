c640a2adcc815f5a78fbf90805d50373f94721f6
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-04-16 13:20:08 +0900

    feat : Add DTO

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -20,7 +20,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import static com.sprint.mission.discodeit.JavaApplication.*;
 
 
 @SpringBootApplication
