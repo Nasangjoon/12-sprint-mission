69ee1b097950adc06631219f9ee4f9e49c4ae5c4
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-04-16 13:05:54 +0900

    feat : Add entity about Read,User status and Binary

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.UUID;
+
+public class BinaryContent implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private UUID id;
+    private String fileName;
+    private String contentType;
+    private byte[] bytes;
+    private Long size;
+    private Instant createdAt;
+
+    public BinaryContent(String fileName, Long size, String contentType, byte[] bytes) {
+        this.id = UUID.randomUUID();
+        this.fileName = fileName;
+        this.contentType = contentType;
+        this.bytes = bytes;
+        this.size = size;
+        this.createdAt = Instant.now();
+    }
+}
