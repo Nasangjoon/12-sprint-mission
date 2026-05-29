7c4282416e9cb88290d9e40fce6bdca31122151e
NaSangJoon <107744931+Nasangjoon@users.noreply.github.com>
2026-05-29 14:24:48 +0900

    fix: delete annotation

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController implements AuthApi {
 
-  // init
   private final AuthService authService;
 
   @PostMapping(path = "login")
